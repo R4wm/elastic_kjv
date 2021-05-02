58e4b8b35403ce5bb8df8a972208e2e6fd266fdf
r4wm <raymondmintz11@gmail.com>
2021-05-01 20:59:23 -0700

    adding replica count to bash script and update es7Bulk.json

--- data/create_kjv_es_db.sh
+++ data/create_kjv_es_db.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
 
-curl -XPOST -H"Content-Type: application/json" http://localhost:9200/kjv/_bulk --data-binary @esBulk.json
+index="kjv"
+
+echo "setting search as you type"
+curl -X PUT "localhost:9200/$index?pretty" -H 'Content-Type: application/json' -d'
+{
+  "mappings": {
+    "properties": {
+      "verse": {
+        "type": "search_as_you_type"
+      }
+    }
+  }
+}
+'
+
+echo "Setting replica count to 1"
+curl -X PUT "localhost:9200/$index/_settings?pretty" -H 'Content-Type: application/json' -d'
+{
+  "index" : {
+    "number_of_replicas" : 1
+  }
+}
+'
+
+
+curl -XPOST -H"Content-Type: application/json" http://localhost:9200/kjv/_bulk --data-binary @es7Bulk.json
 
