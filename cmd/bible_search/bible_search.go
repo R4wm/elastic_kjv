45b3b1ede24b5ab3c7394a02925a6f812997692a
r4wm <raymondmintz11@gmail.com>
2021-05-08 20:46:53 -0700

    refactor: use the func Luke..

--- cmd/bible_search/bible_search.go
+++ cmd/bible_search/bible_search.go
@@ -1,83 +1,17 @@
 package main
 
 import (
-	"bytes"
-	"context"
-	"encoding/json"
 	"flag"
 	"fmt"
-	"log"
 
-	"github.com/elastic/go-elasticsearch/v7"
 	"github.com/r4wm/elastic_kjv/query"
 )
 
 func main() {
-	var resultSize = flag.Int("size", 10, "set results count limit")
+	var resultSize = flag.Int64("size", 10, "set results count limit")
 	var jsonOut = flag.Bool("json", false, "output json")
 	flag.Parse()
-	// stringArg := strings.Join(flag.Args()[:], " ")
-	// query := map[string]interface{}{
-	// 	"size": *resultSize,
-	// 	"query": map[string]interface{}{
-	// 		"match": map[string]interface{}{
-	// 			"text": stringArg,
-	// 		},
-	// 	},
-	// }
 
-	es, _ := elasticsearch.NewDefaultClient()
-	// log.Println(elasticsearch.Version)
-	// log.Println(es.Info())
-
-	var buf bytes.Buffer
-	query := map[string]interface{}{
-		"size": *resultSize,
-		"query": map[string]interface{}{
-			"query_string": map[string]interface{}{
-				"default_field": "text",
-				"query":         query.GetQueryString(flag.Args()[:]),
-			},
-		},
-	}
-
-	if err := json.NewEncoder(&buf).Encode(query); err != nil {
-		log.Fatalf("Error encoding query: %s", err)
-	}
-
-	res, err := es.Search(
-		es.Search.WithContext(context.Background()),
-		es.Search.WithIndex("bible"),
-		es.Search.WithBody(&buf),
-		es.Search.WithTrackTotalHits(true),
-		es.Search.WithPretty(),
-	)
-
-	if err != nil {
-		log.Fatalf("Error getting response: %s\n", err)
-	}
-
-	defer res.Body.Close()
-
-	var r map[string]interface{}
-	if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
-		log.Printf("Error parsint the response body: %s", err)
-	}
-
-	// json out
-	if *jsonOut {
-		jsonResult, _ := json.Marshal(r["hits"])
-		fmt.Println(string(jsonResult))
-		return
-	}
-
-	// plain text out
-	for _, hit := range r["hits"].(map[string]interface{})["hits"].([]interface{}) {
-		book := hit.(map[string]interface{})["_source"].(map[string]interface{})["book"]
-		text := hit.(map[string]interface{})["_source"].(map[string]interface{})["text"]
-		chapter := hit.(map[string]interface{})["_source"].(map[string]interface{})["chapter"]
-		verse := hit.(map[string]interface{})["_source"].(map[string]interface{})["verse"]
-		fmt.Printf("%s %d:%d\n %s\n\n", book, int(chapter.(float64)), int(verse.(float64)), text)
-		// log.Printf("* ID=%s, %s", hit.(map[string]interface{})["_id"], hit.(map[string]interface{})["_source"])
-	}
+	result := query.MakeAndQuery(flag.Args()[:], *resultSize, *jsonOut)
+	fmt.Println(string(result))
 }
--- query/query_string.go
+++ query/query_string.go
@@ -1,8 +1,14 @@
 package query
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
 	"fmt"
+	"log"
 	"strings"
+
+	"github.com/elastic/go-elasticsearch"
 )
 
 // returns query_string: "text: just AND text: faith AND text: alone"
@@ -22,4 +28,70 @@ func GetQueryString(keywords []string) string {
 	}
 
 	return b.String()
+}
+
+func PlainOut(hit map[string]interface{}) []byte {
+
+	var buf bytes.Buffer
+	for _, hit := range hit["hits"].(map[string]interface{})["hits"].([]interface{}) {
+		book := hit.(map[string]interface{})["_source"].(map[string]interface{})["book"]
+		text := hit.(map[string]interface{})["_source"].(map[string]interface{})["text"]
+		chapter := hit.(map[string]interface{})["_source"].(map[string]interface{})["chapter"]
+		verse := hit.(map[string]interface{})["_source"].(map[string]interface{})["verse"]
+
+		buf.WriteString(fmt.Sprintf("%s %d:%d\n %s\n\n", book, int(chapter.(float64)), int(verse.(float64)), text))
+	}
+	return buf.Bytes()
+}
+
+func MakeAndQuery(aWords []string, aSize int64, aJson bool) []byte {
+	var buf bytes.Buffer
+
+	es, _ := elasticsearch.NewDefaultClient()
+	parsedQueryString := GetQueryString(aWords)
+	log.Print("parsedQuerystring: ", parsedQueryString)
+
+	query := map[string]interface{}{
+		"size": aSize,
+		"query": map[string]interface{}{
+			"query_string": map[string]interface{}{
+				"default_field": "text",
+				"query":         GetQueryString(aWords),
+			},
+		},
+	}
+
+	if err := json.NewEncoder(&buf).Encode(query); err != nil {
+		log.Fatalf("Error encoding query: %s", err)
+	}
+
+	res, err := es.Search(
+		es.Search.WithContext(context.Background()),
+		es.Search.WithIndex("bible"),
+		es.Search.WithBody(&buf),
+		es.Search.WithTrackTotalHits(true),
+		es.Search.WithPretty(),
+	)
+
+	if err != nil {
+		log.Fatalf("Error getting response: %s\n", err)
+	}
+
+	defer res.Body.Close()
+
+	var r map[string]interface{}
+	if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
+		log.Print("Failed to get json decode response from ES")
+	}
+
+	jsonResult, err := json.Marshal(r["hits"])
+	if err != nil {
+		log.Fatalf("Failed to parse es query result: %v\n", err)
+	}
+	if aJson {
+		return jsonResult
+	}
+
+	return PlainOut(r)
+
 }
